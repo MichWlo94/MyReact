3f65ddba48cdd6b87a7aa1246b0da3a2233d2a43
mwlodarczyk <mwlodarczyk94@outlook.com>
2023-08-31 15:53:36 +0200

    app with validation, to be continued

---
+++ src/ComponentsProject4/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>
+        <button type='submit' className="px-4 py-2 mt-4 text-white bg-gray-800 rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700">Submit</button>
+    </div>
+  )
+}
+
+export default Button
